adb09307e2d19d2b970720c0f822cc34e6f0ce1c
Name <fasyaraihan82@gmail.com>
2025-05-05 19:08:09 +0700

    cors

--- presentia-backend/config/cors.php
+++ presentia-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://presentia.matradipti.org', env('FRONTEND_URL'), '*'],
+    'allowed_origins' => ['http://presentia.matradipti.org', 'http://localhost:8080' ,env('FRONTEND_URL'), '*'],
 
     'allowed_origins_patterns' => [],
 
