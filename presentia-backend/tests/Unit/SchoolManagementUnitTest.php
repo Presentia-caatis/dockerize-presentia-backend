6a4339d2f262c588ebb8cb1de76baa1efb19de7c
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2025-05-24 15:52:34 +0700

    school management tests update

--- presentia-backend/tests/Unit/SchoolManagementUnitTest.php
+++ presentia-backend/tests/Unit/SchoolManagementUnitTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Student;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Permission\Models\Role;
@@ -21,111 +22,76 @@ class SchoolManagementUnitTest extends TestCase
 {
     use RefreshDatabase, TestCaseHelpers;
 
-    // #[Test]
-    // public function user_can_register_as_staff_with_valid_token(): void
-    // {
-    //     $school = School::factory()->create([
-    //         'school_token' => Str::upper(Str::random(10)),
-    //     ]);
+    #[Test]
+    public function user_can_register_as_staff_with_valid_token(): void
+    {
+        $this->authUser->update(['school_id' => null]);  
 
-    //     $user = User::factory()->create([
-    //         'password'  => bcrypt('Password123!'),
-    //         'school_id' => null,
-    //     ]);
+        $school = School::factory()->create([
+            'school_token' => Str::upper(Str::random(10)),
+        ]);
 
-    //     $loginRes = $this->postJson('/api/login', [
-    //         'email_or_username' => $user->email,
-    //         'password'          => 'Password123!',
-    //     ])->assertStatus(200)
-    //       ->assertJsonStructure(['token']);
+        $response = $this->postJson('/api/user/school/assign-via-token', [
+            'school_token' => $school->school_token,
+        ]);
 
-    //     $token = $loginRes->json('token');
+        if ($response->status() === 500) {
+            dd($response->json(), $response->exception->getMessage(), $response->exception->getTraceAsString());
+        }
 
-    //     $response = $this->withHeaders([
-    //             'Authorization' => 'Bearer ' . $token,
-    //         ])->postJson('/api/school/assign-via-token', [
-    //             'school_token' => $school->school_token,
-    //         ]);
+        $response->assertStatus(201)
+                 ->assertJson([
+                     'status'  => 'success',
+                     'message' => 'User assigned to school successfully',
+                     'data'    => [
+                         'id'     => $this->authUser->id, 
+                         'school' => [
+                             'id'   => $school->id,
+                             'name' => $school->name,
+                         ],
+                     ],
+                 ]);
 
-    //     $response->assertStatus(201)
-    //              ->assertJson([
-    //                  'status'  => 'success',
-    //                  'message' => 'User assigned to school successfully',
-    //                  'data'    => [
-    //                      'id'     => $user->id,
-    //                      'school' => [
-    //                          'id'   => $school->id,
-    //                          'name' => $school->name,
-    //                      ],
-    //                  ],
-    //              ]);
-
-    //     $this->assertDatabaseHas('users', [
-    //         'id'        => $user->id,
-    //         'school_id' => $school->id,
-    //     ]);
-    // }
+        $this->assertDatabaseHas('users', [
+            'id'        => $this->authUser->id, 
+            'school_id' => $school->id,
+        ]);
+    }
     
-    // #[Test]
-    // public function system_rejects_staff_registration_with_invalid_token(): void
-    // {
-    //     $user = User::factory()->create([
-    //         'password' => bcrypt('Password123!'),
-    //     ]);
+    #[Test]
+    public function system_rejects_staff_registration_with_invalid_token(): void
+    {
+        $this->authUser->update(['school_id' => null]);
 
-    //     $token = $this->postJson('/api/login', [
-    //         'email_or_username' => $user->email,
-    //         'password'          => 'Password123!',
-    //     ])->json('token');
+        $response = $this->postJson('/api/user/school/assign-via-token', [
+            'school_token' => 'INVALIDTOKEN',
+        ]);
 
-    //     $response = $this->withHeaders([
-    //             'Authorization' => 'Bearer ' . $token,
-    //         ])->postJson('/api/school/assign-via-token', [
-    //             'school_token' => 'INVALIDTOKEN',
-    //         ]);
+        $response->assertStatus(422)
+                 ->assertJsonValidationErrors(['school_token']);
 
-    //     $response->assertStatus(422)
-    //              ->assertJsonValidationErrors(['school_token']);
+        $this->assertDatabaseHas('users', [
+            'id'        => $this->authUser->id,
+            'school_id' => null,
+        ]);
+    }
 
-    //     $this->assertDatabaseHas('users', [
-    //         'id'        => $user->id,
-    //         'school_id' => null,
-    //     ]);
-    // }
+    #[Test]
+    public function user_can_access_managed_school_information()
+    {
+        $school = School::factory()->create();
 
-    // #[Test]
-    // public function admin_can_retrieve_role_list(): void
-    // {
-    //     /* ---------- Arrange ---------- */
-    //     // 1. Buat role admin + permissions (jika belum)
-    //     $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
-    //     $adminRole->givePermissionTo('manage_school_users'); // izin minimal
+        $this->authUser->update(['school_id' => $school->id]);
 
-    //     $adminUser = User::factory()->create(['password' => bcrypt('Password123!')]);
-    //     $adminUser->assignRole($adminRole);
+        Student::factory()->count(5)->create(['is_active' => true, 'school_id' => $school->id]);
 
-    //     $token = $this->postJson('/api/login', [
-    //         'email_or_username' => $adminUser->email,
-    //         'password'          => 'Password123!',
-    //     ])->json('token');
+        $response = $this->getJson('/api/dashboard-statistic/static');
 
-    //     Role::factory()->count(2)->create();
-
-    //     /* ---------- Act ---------- */
-    //     $response = $this->withHeaders(['Authorization' => 'Bearer ' . $token])
-    //                      ->getJson('/api/role?perPage=10');
-
-    //     /* ---------- Assert ---------- */
-    //     $response->assertStatus(200)
-    //              ->assertJson([
-    //                  'status'  => 'success',
-    //                  'message' => 'Roles retrieved successfully',
-    //              ])
-    //              ->assertJsonStructure(['data' => ['data' => [['id','name','permissions']]]]);
-
-    //     // Pastikan tidak kosong
-    //     $this->assertGreaterThan(0, count($response->json('data.data')));
-    // }
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+             'active_students' => 5,
+            ]);
+    }
 
     #[Test]
     public function superadmin_can_retrieve_school_list()
