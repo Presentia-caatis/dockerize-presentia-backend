cbabe6966bf1bef4c0fa5cda2cb5b1282909be72
Name <fasyaraihan82@gmail.com>
2025-07-12 23:53:17 +0700

    create model and migration of Enrollment

---
+++ presentia-backend/app/Models/Enrollment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\BelongsToSchool;
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    use BelongsToSchool;
+    protected $fillable = [
+        'semester_id',
+        'class_group_id',
+        'student_id',
+        'school_id',
+    ];
+
+    // Relationships
+
+    public function semester()
+    {
+        return $this->belongsTo(Semester::class);
+    }
+
+    public function classGroup()
+    {
+        return $this->belongsTo(ClassGroup::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+}
